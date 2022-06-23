12a5b5e5ea0f602bf67f7b7a284dde05ea39550f
Jose Villa <jgvillaperez@student.fullsail.educd>
2022-06-23 00:21:12 -0600

    updated homepage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 // style sheet import
-import './App.css';
+import "./App.css";
 
 // react browser import
-import { HashRouter as Router } from 'react-router-dom' 
+import { BrowserRouter as Router } from "react-router-dom";
 
 // component imports
-import Header from './components/Header'
-import Routes from './components/Routes'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Routes from "./components/Routes";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <Router>
       <div className="App" style={styles.container}>
-        <Header/>
-          <main>
-            <Routes />
-          </main> 
-        <Footer/>
+        <Header />
+        <main>
+          <Routes />
+        </main>
+        <Footer />
       </div>
     </Router>
-
-  )
+  );
 }
 
 export default App;
 
 const styles = {
   container: {
-    position: 'relative',
-    minHeight: '100vh'
-  }
-}
+    position: "relative",
+    minHeight: "100vh",
+  },
+};
